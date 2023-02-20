a3c822d4dabbc2c0ddbfc920287f713aa56b865f
anchaapedro <anchaapedro@gmail.com>
2023-02-20 14:35:42 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main function
  *
- * Return: 0
+ * Return: Always 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
